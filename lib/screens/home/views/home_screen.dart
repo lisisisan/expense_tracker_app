25cb9cc31dab11fe9e6f9e6dd33079a413930e55
lisisisan <lisisisan18@gmail.com>
2025-01-06 22:51:17 +0500

    feat: add top info into main screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expense_tracker_app/screens/home/views/main_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -60,6 +61,7 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: const MainScreen(),
     );
   }
 }
