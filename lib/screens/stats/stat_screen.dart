fd60d01b09c18f77080c7a720b901780c3139cfc
lisisisan <lisisisan18@gmail.com>
2025-01-07 21:06:47 +0500

    feat: add stat diagram

--- lib/screens/stats/stat_screen.dart
+++ lib/screens/stats/stat_screen.dart
@@ -1,18 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expense_tracker_app/screens/stats/chart.dart';
+import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
   const StatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
         child: Column(
           children: [
-            Text(
+            const Text(
               'Transactions',
               style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20.0),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+                child: MyChart(),
+              ),
             ),
           ],
         ),
